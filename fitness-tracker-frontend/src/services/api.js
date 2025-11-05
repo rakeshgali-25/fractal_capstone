ade8cd48110ad8c9e2373e68349e50fae516d64b
Danish Ahmad <samdanyshh@gmail.com>
2025-11-05 10:07:34 +0000

    added validation ot activity

--- fitness-tracker-frontend/src/services/api.js
+++ fitness-tracker-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:7000/',
+  baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:53394/',
   timeout: 10000,
 });
 
